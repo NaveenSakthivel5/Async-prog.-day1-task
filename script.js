15b5c9cf38d4085dbefa6bd4e784c94364f48263
Naveen S <130961168+NaveenSakthivel5@users.noreply.github.com>
2023-11-18 20:26:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let countdown = 10;
+
+setTimeout(function () {
+  document.getElementById('countdown').innerText = countdown;
+  countdown--;
+
+  setTimeout(function () {
+    document.getElementById('countdown').innerText = countdown;
+    countdown--;
+
+    setTimeout(function () {
+      document.getElementById('countdown').innerText = countdown;
+      countdown--;
+
+      setTimeout(function () {
+        document.getElementById('countdown').innerText = countdown;
+        countdown--;
+
+        setTimeout(function () {
+          document.getElementById('countdown').innerText = countdown;
+          countdown--;
+
+          setTimeout(function () {
+            document.getElementById('countdown').innerText = countdown;
+            countdown--;
+
+            setTimeout(function () {
+              document.getElementById('countdown').innerText = countdown;
+              countdown--;
+
+              setTimeout(function () {
+                document.getElementById('countdown').innerText = countdown;
+                countdown--;
+                
+                setTimeout(function () {
+                    document.getElementById('countdown').innerText = countdown;
+                    countdown--;
+
+                    setTimeout(function () {
+                        document.getElementById('countdown').innerText = countdown;
+                        countdown--;
+
+                        setTimeout(function () {
+                            document.getElementById('countdown').innerText = "Happy Independence Day";
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
